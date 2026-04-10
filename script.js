52f80c0df35c82e3f903699e02584238e90e7fce
TovaR2029 <tova.rehbein2029@gbwl.org>
2026-04-10 14:55:27 +0000

    variables!!

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-console.log("hello world");
+// This is a SINGLE-LINE COMMENT
+
+/* This is a MULTI-LINE COMMENT 
+The code below is an example STATEMENT )instruction/command)
+Every statement ends with a SEMICOLON
+*/
+console.log("Welcome to JS!");
+
+// VARIABLES are named storage containers for data
+
+// DECLARE a variable using the keyword LET
+let username;
+
+//ASSIGN a value to a variable
+username = "Tova Rehbein";
+
+// DECLARE + ASSIGN in one line
+let pokemon = "pikachu";
+let luckyNum = 5;
+luckyNum = 13;
+
+// See the value stored in a variable
+console.log(username);
 
