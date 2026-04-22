e6db59ae95b99aaa60f7fa96612d111cb9a5750b
TovaR2029 <tova.rehbein2029@gbwl.org>
2026-04-22 14:25:30 +0000

    input, output

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ console.log("My age in dog years is " + ageDogYears)
 console.log("I just used a function!");
 // some functions need PARAMETERS (input)
 
-// DEFINE (create) your own function
+// DECLARE (create) your own function
 function skincare() {
     // STATEMENTS (actions) to execute for this routine
     console.log("1. Cleanse");
@@ -54,3 +54,21 @@ function skincare() {
 }
 // CALL the function to use it!
 skincare();
+skincare(); // can run the function again
+
+// 4 types of FUNCTIONS:
+// input - yes or no? output - yes or no?
+
+// SANDWICH - need input, provide output
+// If you need input, list the (parameters)
+function makeSandwich(filling, sauce, bread) {
+    // put the ingredients together
+    let sandwich;
+    sandwich = filling + " and " + sauce + " on " + bread;
+    // if you give output, use the word RETURN
+    return sandwich; 
+}
+
+// using a function with input mean YOU need to provide arguments in the parenthesis
+let mySandwich = makeSandwich("mozzarella", "pesto", "baguette");
+console.log(mySandwich);
